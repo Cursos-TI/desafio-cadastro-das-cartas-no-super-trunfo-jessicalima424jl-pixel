b6b09268df4e42a195bbc669f02d8d6b5ee134d3
jessicalima424jl-pixel <jessicalima424.jl@gmail.com>
2025-09-04 21:10:31 -0300

    Update supertrunfo.c

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,1 +1,60 @@
 
+#include <stdio.h>
+int main (){
+    char cidade [50];
+    int população;
+    float Area;
+    float Pib;
+    int Pontos, turisticos;
+printf("whashington");
+scanf("%s",&cidade);
+
+printf("375,000");
+scanf("%f",&Pib);
+
+printf("177 km");
+scanf("%f",&Area);
+
+printf("678.972");
+scanf("%d",&população);
+
+ printf ("6");
+ scanf ("%d",&Pontos,turisticos);
+ printf ("cidade %s");
+ printf ("populaçao %d");
+ printf ("area %f");
+ printf ("pib %f");
+ printf ("pontos turisticos %d");
+ return 0;
+
+
+#include <stdio.h>
+int main (){
+    char cidade [50];
+    int população;
+    float Area;
+    float Pib;
+    int Pontos, turisticos;
+printf("Minas gerai");
+scanf("%s",&cidade);
+
+printf("275,000");
+scanf("%f",&Pib);
+
+printf("876.000 km");
+scanf("%f",&Area);
+
+printf("21,004,000");
+scanf("%d",&população);
+
+ printf ("15");
+ scanf ("%d",&Pontos,turisticos);
+ printf ("cidade %s");
+ printf ("populaçao %d");
+ printf ("area %f");
+ printf ("pib %f");
+ printf ("pontos turisticos %d");
+ return 0;
+
+
+}
